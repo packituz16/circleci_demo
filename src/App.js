1670181e65709a076d0f7cb6a878a7bfd890a73f
packituz <packituz16@gmail.com>
2020-08-28 00:06:06 -0500

    Implemented CircleCI Integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import * as config from "./config";
+
+const { tenantName, backgroundColor } = config;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header" style={{ backgroundColor }}>
+                <p data-testid="companyName">{tenantName}</p>
+            </header>
+            <footer className="Footer" data-testid="companyFooter">
+                2020. By Awesome Company
+            </footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    tenantName: process.env.REACT_APP_TENANT,
+    backgroundColor: process.env.REACT_APP_BACKGROUND_COLOR,
+};
